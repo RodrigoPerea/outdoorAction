bca9d7a0d98eae96ffee8e2f7cb2ffcf70a7c230
Christian Rodrigo <chri65mb@stud.kea.dk>
2022-12-13 17:45:57 +0100

    jek

---
+++ priser.js
@@ -0,0 +1,7 @@
+let atvpopup = document.getElementById("kontakt-pop-op");
+function openPopup1() {
+  kontaktPopOp.classList.add("open-popup");
+}
+function closePopup1() {
+  kontaktPopOp.classList.remove("open-popup");
+}

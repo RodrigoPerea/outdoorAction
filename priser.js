c0c89c271b73a120c934b2c9e601255d140bcbbe
Malte Jensen <malt656c@stud.kea.dk>
2022-12-14 11:46:51 +0100

    popup virker

--- priser.js
+++ priser.js
@@ -1,5 +1,6 @@
-let kontaktPopOp = document.getElementById("kontakt-pop-op");
+let kontaktPopOp = document.querySelector("#kontakt-pop-op");
 function openPopup1() {
+  console.log("open")
   kontaktPopOp.classList.add("open-popup");
 }
 function closePopup1() {

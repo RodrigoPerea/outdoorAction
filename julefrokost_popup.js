80d323857e4823f0e5d4d5dbc079dc598ab92864
Malte Jensen <malt656c@stud.kea.dk>
2022-12-14 17:35:33 +0100

    lkj

--- julefrokost_popup.js
+++ julefrokost_popup.js
@@ -1,10 +1,13 @@
 
 const julefrokost_banner = document.querySelector(".julefrokost_banner")
+const julefrokost_video = document.querySelector("#julefrokost_video")
 
 function julefrokost_popup(){
     if (julefrokost_banner.classList.contains("vis_banner")) {
         julefrokost_banner.classList.remove("vis_banner");
+        julefrokost_video.pause();
       } else {
         julefrokost_banner.classList.add("vis_banner");
+        julefrokost_video.play();
       }
 }
